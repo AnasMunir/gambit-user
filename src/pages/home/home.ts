71041ea174cecf84787508ca02ecbee014788bbd
AnasMunir <anas.munir.92@gmail.com>
2016-11-07 18:37:37 +0500

    added logout function in home.ts, now testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AuthData } from '../../providers/auth-data';
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public authData: AuthData) {
+
+  }
+  logoutUser() {
+    this.authData.logoutUser().then( () => {
+      this.navCrtl.root(LoginPage);
+    });
   }
 
 }
